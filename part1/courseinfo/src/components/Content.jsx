9e00e361056718dfbaa141065e119807e91a7013
Atharva Pardeshi <atharva.exe@gmail.com>
2023-03-11 10:24:24 +0530

    feat: exercise 1.3     refactor: move parts info in separate objects     refactor: update the app to use new props format

--- part1/courseinfo/src/components/Content.jsx
+++ part1/courseinfo/src/components/Content.jsx
@@ -1,10 +1,10 @@
 import Part from "./Part"
 
-const Content = ({parts, exercises}) => (
+const Content = ({parts}) => (
   <>
-    <Part part={parts[0]} exercise={exercises[0]} />
-    <Part part={parts[0]} exercise={exercises[0]} />
-    <Part part={parts[0]} exercise={exercises[0]} />
+    <Part part={parts[0].name} exercise={parts[0].exercises} />
+    <Part part={parts[1].name} exercise={parts[1].exercises} />
+    <Part part={parts[2].name} exercise={parts[2].exercises} />
   </>
 )
 
