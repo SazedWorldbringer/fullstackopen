ff5c585812ddd7d8b2809cf9c83e0375600dda21
Atharva Pardeshi <atharva.exe@gmail.com>
2023-03-28 17:22:55 +0530

    feat: exercise 1.12, display random anecdote

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -14,9 +14,16 @@ function App() {
 
   const [selected, setSelected] = useState(0);
 
+  const handleClick = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomIndex)
+    console.log(randomIndex, anecdotes[randomIndex])
+  }
+
   return (
     <div>
-      {anecdotes[selected]}
+      <p>{anecdotes[selected]}</p>
+      <button onClick={handleClick}>next anecdote</button>
     </div>
   )
 }
