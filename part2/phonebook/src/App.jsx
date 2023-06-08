f8b29d60efb3ffcddc50c25b484865c0b7c21045
Atharva Pardeshi <atharva.exe@gmail.com>
2023-06-08 09:49:15 +0530

    refactor: rename persons to people everywhere because grammar @_@

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
-import Persons from './components/Persons'
-import personService from './services/persons'
+import People from './components/People'
+import personService from './services/people'
 
 function App() {
-  const [persons, setPersons] = useState([])
-  const [filteredPersons, setFilteredPersons] = useState([])
+  const [people, setPeople] = useState([])
+  const [filteredPeople, setFilteredPeople] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [notification, setNotification] = useState('')
@@ -14,9 +14,9 @@ function App() {
   useEffect(() => {
     personService
       .getAll()
-      .then(initialPersons => {
-        setPersons(initialPersons);
-        setFilteredPersons(initialPersons);
+      .then(initialPeople => {
+        setPeople(initialPeople);
+        setFilteredPeople(initialPeople);
       })
   }, [])
 
@@ -28,15 +28,15 @@ function App() {
     setNewNumber(event.target.value);
   }
 
-  // Check if the persons array contains new person
-  const containsPerson = (persons, person) => {
-    for (let i = 0; i < persons.length; i++) {
+  // Check if the people array contains new person
+  const containsPerson = (people, person) => {
+    for (let i = 0; i < people.length; i++) {
       // If any person's name in the array matches new person's name,
       // but the number doesn't match
       // update the person's number and return
-      if (persons[i].name.toLowerCase() === person.name.toLowerCase()) {
-        if (persons[i].number !== person.number) {
-          const updateTheNumber = updateNumber(person, persons[i].id)
+      if (people[i].name.toLowerCase() === person.name.toLowerCase()) {
+        if (people[i].number !== person.number) {
+          const updateTheNumber = updateNumber(person, people[i].id)
           if (updateTheNumber) return true
         }
         // If the user doesn't want to update the number, clear input values and return
@@ -61,16 +61,16 @@ function App() {
 
     const newPerson = { name: newName, number: newNumber }
 
-    // check if the person is already in the persons array + update the number
-    if (containsPerson(persons, newPerson)) return;
+    // check if the person is already in the people array + update the number
+    if (containsPerson(people, newPerson)) return;
 
     // Create a new person if one isn't already present
     personService
       .create(newPerson)
       .then(returnedPerson => {
-        // update persons array
-        setPersons(persons.concat(returnedPerson));
-        setFilteredPersons(filteredPersons.concat(returnedPerson));
+        // update people array
+        setPeople(people.concat(returnedPerson));
+        setFilteredPeople(filteredPeople.concat(returnedPerson));
       })
 
     setNotification(`Added ${newName}`)
@@ -95,14 +95,14 @@ function App() {
   }
 
   const deleteName = (id) => {
-    if (!window.confirm(`Delete ${persons.find(person => person.id === id).name}?`)) return;
+    if (!window.confirm(`Delete ${people.find(person => person.id === id).name}?`)) return;
 
     // delete person with given id
     personService.deleteName(id)
 
-    // update persons array
-    setPersons(persons.filter(person => person.id !== id))
-    setFilteredPersons(persons.filter(person => person.id !== id))
+    // update people array
+    setPeople(people.filter(person => person.id !== id))
+    setFilteredPeople(people.filter(person => person.id !== id))
   }
 
   const updateNumber = (obj, id) => {
@@ -112,9 +112,9 @@ function App() {
     personService
       .update(id, obj)
       .then(returnedPerson => {
-        // update persons array
-        setPersons(persons.filter(person => person.id !== id ? person : returnedPerson))
-        setFilteredPersons(persons.filter(person => person.id !== id ? person : returnedPerson))
+        // update people array
+        setPeople(people.filter(person => person.id !== id ? person : returnedPerson))
+        setFilteredPeople(people.filter(person => person.id !== id ? person : returnedPerson))
       })
 
     // clear input values
@@ -124,11 +124,11 @@ function App() {
   }
 
   const handleSearchChange = (event) => {
-    // filter the person array so that it contains persons with names matching the input value
-    const filtered = persons.filter(
+    // filter the person array so that it contains people with names matching the input value
+    const filtered = people.filter(
       person => person.name.toLowerCase().includes(event.target.value.toLowerCase())
     );
-    setFilteredPersons(filtered);
+    setFilteredPeople(filtered);
   }
 
   return (
@@ -143,7 +143,7 @@ function App() {
       <PersonForm handleSubmit={addName} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} newName={newName} newNumber={newNumber} />
 
       <h3>Numbers</h3>
-      <Persons handleDelete={deleteName} persons={filteredPersons} />
+      <People handleDelete={deleteName} people={filteredPeople} />
     </div>
   )
 }
