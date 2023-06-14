f1139fda95c35d2f20b4c1126704f1001c4102f6
Atharva Pardeshi <atharva.exe@gmail.com>
2023-06-15 00:21:46 +0530

    refactor: move blog schema to models directory

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,18 +1,10 @@
 const express = require('express')
 const cors = require('cors')
-const mongoose = require('mongoose')
 require('dotenv').config()
+const mongoose = require('mongoose')
+const Blog = require('./models/blog')
 
 const app = express()
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
