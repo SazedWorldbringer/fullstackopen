8014fd212fc7dd5bb08014d908ce76d45db44ae5
Atharva Pardeshi <atharva.exe@gmail.com>
2023-06-15 13:22:12 +0530

    feat: return the total likes of a blog list

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -3,9 +3,15 @@ const dummy = (blog) => {
 }
 
 const totalLikes = (blogs) => {
-  if(blogs.length === 0) return 0
-  if(blogs.length === 1) return blogs[0].likes
+  if (blogs.length === 0) return 0
+  if (blogs.length === 1) return blogs[0].likes
+
+  const likes = blogs.map(blog => blog.likes)
+  const totalLikes = likes.reduce((sum, curr) => sum + curr, 0)
+
+  return totalLikes
 }
+
 
 module.exports = {
   dummy,
