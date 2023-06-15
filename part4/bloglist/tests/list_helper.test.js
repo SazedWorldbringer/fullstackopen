c3a180bf8e2046f3514c6ca54af76760481c644f
Atharva Pardeshi <atharva.exe@gmail.com>
2023-06-15 13:09:54 +0530

    test: test for totalLikes function when passed only one blog post

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -14,4 +14,20 @@ describe('total likes', () => {
     const result = listHelper.totalLikes(listWithNoBlogs)
     expect(result).toBe(0)
   })
+
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  test('when list has only one blog, equal the likes of that blog', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
 })
