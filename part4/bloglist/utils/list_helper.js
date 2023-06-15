6590f5b13109c3920ae03fcb79fad68e48112e81
Atharva Pardeshi <atharva.exe@gmail.com>
2023-06-15 13:23:47 +0530

    feat: exercise 4.4
    write helper function(totalLikes) and unit tests for it

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -3,9 +3,12 @@ const dummy = (blog) => {
 }
 
 const totalLikes = (blogs) => {
+  // return 0 if the no blogs are passed
   if (blogs.length === 0) return 0
+  // if one blog is passed, return it's likes
   if (blogs.length === 1) return blogs[0].likes
 
+  // calculate total likes and return for a normal blog list
   const likes = blogs.map(blog => blog.likes)
   const totalLikes = likes.reduce((sum, curr) => sum + curr, 0)
 
