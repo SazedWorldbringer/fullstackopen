70a37a3a27a6c86b058ada920923907443644f8d
Atharva Pardeshi <atharva.exe@gmail.com>
2023-06-18 20:11:20 +0530

    test: test favoriteBlog function when the bloglist passed as a parameter contains only one blog object

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -95,4 +95,26 @@ describe('most likes', () => {
     const result = listHelper.favoriteBlog(listWithNoBlogs)
     expect(result).toBe(null)
   })
+
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const expectedResult = {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    likes: 5
+  }
+
+  test('when list has only one blog, return the same blog object', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    expect(result).toEqual(expectedResult)
+  })
 })
