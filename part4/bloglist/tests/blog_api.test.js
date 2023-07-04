056d8eda9cf7da767a06b23fa97a55957791a262
Atharva Pardeshi <atharva.exe@gmail.com>
2023-07-04 15:49:42 +0530

    test: reset th db before each test, test if correct number of blogs are returned on a get request

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
-const supertest = require('supertest')
-const app = require('../app')
+const supertest = require('supertest');
+const app = require('../app');
 
-const api = supertest(app)
+const api = supertest(app);
+
+const Blog = require('../models/blog');
+const helper = require('./test_helper');
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  await Blog.insertMany(helper.initialBlogs)
+}, 100000)
 
 test('blogs are returned as json', async () => {
   await api
@@ -11,8 +19,12 @@ test('blogs are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 }, 100000)
 
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
 afterAll(async () => {
   await mongoose.connection.close()
 })
-
-
