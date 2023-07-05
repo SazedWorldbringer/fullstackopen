6415dd640a2691db953a32eba3d32ba3dd4d7dba
Atharva Pardeshi <atharva.exe@gmail.com>
2023-07-05 19:55:20 +0530

    test: test for deletion of individual blogpost

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -93,6 +93,17 @@ describe('adding new blogs', () => {
   }, 100000)
 })
 
+describe('deletion of a blog', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
+
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+  })
+})
+
 afterAll(async () => {
   await mongoose.connection.close()
 })
