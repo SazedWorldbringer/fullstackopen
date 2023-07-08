d92736455dd84971323d2b5a4cd4e741e73e7f5d
Atharva Pardeshi <atharva.exe@gmail.com>
2023-07-08 22:37:29 +0530

    feat: exercise 4.15 feat: use usersRouter for handing users endpoint

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const config = require('./utils/config')
 
 const blogsRouter = require('./controllers/blogslist')
+const usersRouter = require('./controllers/users');
 const mongoose = require('mongoose')
 
 const app = express()
@@ -14,5 +15,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
