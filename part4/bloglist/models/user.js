0e1d15d7bc6a29523f55e75fa4d95d06fad28d16
Atharva Pardeshi <atharva.exe@gmail.com>
2023-07-28 17:48:30 +0530

    chore: use mongose unique validation plugin

--- part4/bloglist/models/user.js
+++ part4/bloglist/models/user.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -11,16 +12,18 @@ const userSchema = new mongoose.Schema({
   passwordHash: String,
 });
 
-userSchema.set('toJSON', {
+userSchema.plugin(uniqueValidator);
+
+userSchema.set("toJSON", {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
     delete returnedObject.__v;
     // do not return the password
     delete returnedObject.passwordHash;
-  }
+  },
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
